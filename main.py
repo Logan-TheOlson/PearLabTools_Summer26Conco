78924ea4a7a96550354b1899de326f625b284d5d
redpandaowl <redpandaowl@gmail.com>
2026-05-26 14:19:19 -0500

    Converted Values

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+df = pd.read_table('InputEx.DAT', delimiter=",", skiprows=34)
 
+with open('InputEx.DAT', 'r') as f:
+    for line in f:
+        if 'SAMPLE_MASS' in line:
+            mass = float(line.strip().split(',')[1])
+            break
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+df = df.iloc[:,[2,3,4]]
+df = df.dropna(subset=['Moment (emu)'])
+df = df.loc[df['Moment (emu)'] != '0']
 
+#Converting to Tesla
+df.iloc[:,1]= df.iloc[:, 1].astype(float) * 10**(-4)
+#Moment to magnetic moment /mass
+df.iloc[:,2]= df.iloc[:, 2].astype(float)/(mass/10**6)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-print("test")
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(df)
+print("The mass of the sample is "+str(mass)+ " mg")
